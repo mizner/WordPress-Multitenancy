2e91603ad3bafe4ebfed2623755578373c1e3bf6
Mizner <mike@mizner.io>
2018-04-01 05:58:51 -0400

    Move sites config
    other minor fixes and refactoring

--- setup/sites.nginx.sh
+++ setup/sites.nginx.sh
@@ -6,11 +6,12 @@
 
 # Requires jq to be installed https://stedolan.github.io/jq/
 
-SITES_OBJECT=$(cat sites.json | jq -r  '.sites')
-SITES_COUNT=$( echo $SITES_OBJECT | jq '. | length' )
+PATH_NGNIX='/etc/nginx'
+PATH_ROOT='/var/www'
+PATH_SITE_DIR='/var/www/public'
 
-PATH_NGNIX_SITES_AVAILABLE='/etc/nginx/sites-available'
-PATH_SITE_DIR='/var/www/public/'
+SITES_OBJECT=$(cat "$PATH_ROOT/sites.json" | jq -r  '.sites')
+SITES_COUNT=$( echo $SITES_OBJECT | jq '. | length' )
 
 function getObjectItem () {
     local row=$1
@@ -21,26 +22,36 @@ function getObjectItem () {
     echo $result
 }
 
-
 function createNgnixBlockFile(){
-    local siteUrl=$1;
-    local path=$PATH_NGNIX_SITES_AVAILABLE;
-    echo $path;
-    # sudo cp "$path/default" "$path/$siteUrl"
+    local siteUrl=$1
+    local pathAvailable="$PATH_NGNIX/sites-available"
+    local pathEnabled="$PATH_NGNIX/sites-enabled"
+    
+    sudo cp "$pathAvailable/example" "$pathAvailable/$siteUrl"
+    sed -i "s|@SITE_URL|$siteUrl|g" "$pathAvailable/$siteUrl"
+    sudo ln -s "$pathAvailable/$siteUrl" "$pathEnabled/"
+}
+
+function createSiteDirectory(){
+    local siteUrl=$1
+    cp -rf "$PATH_SITE_DIR/example" "$PATH_SITE_DIR/$siteUrl"
 }
 
 function siteSetup(){
     local row=$(echo $1 | base64 --decode )
 
-    # local siteUrl=getObjectItem $row url
+    local siteUrl=$(getObjectItem $row url)
 
     # 1. Create Ngnix Block File
-    # DIE createNgnixBlockFile $row
+    createNgnixBlockFile $siteUrl
 
     # 2. Create Site Directory
-    # DIE mkdir "$PATH_SITE_DIR/$siteUrl"
+    createSiteDirectory $siteUrl
 }
 
 for row in $(echo "$SITES_OBJECT" | jq -r '.[]  | @base64'); do
     siteSetup $row
 done
+
+sudo systemctl restart php7.2-fpm
+sudo systemctl restart nginx
