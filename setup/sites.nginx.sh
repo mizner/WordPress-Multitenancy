d84d52b387f59c9789e038cb80c1354b5168912e
Mizner <mike@mizner.io>
2018-04-01 03:02:02 -0400

    Restucture folders
    additions to ngnix site creation

--- setup/sites.nginx.sh
+++ setup/sites.nginx.sh
@@ -6,19 +6,35 @@
 
 # Requires jq to be installed https://stedolan.github.io/jq/
 
-# jq -nc --slurpfile OBJEC sites.json
 SITES_OBJECT=$(cat sites.json | jq -r  '.sites')
 SITES_COUNT=$( echo $SITES_OBJECT | jq '. | length' )
 
+echo $SITES_OBJECT
+exit 1
+
+PATH_NGNIX_SITES_AVAILABLE = '/etc/nginx/sites-available'
+
 function getObjectItem() {
-    local row=$(echo $1 | base64 --decode ) 
+    local row=$1
     local key=$2
 
     local result=$(echo ${row} | jq -r '.url');
-    
+
     echo $result
 }
 
+function createNgnixBlockFile(){
+    local siteUrl=$1
+    local path=$PATH_NGNIX_SITES_AVAILABLE
+    sudo cp "$path/default" "$path/$siteUrl"
+}
+
+function siteSetup(){
+    local row=$(echo $1 | base64 --decode )
+    local url=getObjectItem $row url
+
+}
+
 for row in $(echo "$SITES_OBJECT" | jq -r '.[]  | @base64'); do
-    getObjectItem $row url
+    # siteSetup $row
 done
